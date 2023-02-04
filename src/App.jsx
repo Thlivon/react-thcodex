0c2466984a3baf076661365bf7a28f7437000036
= <raullivon@gmail.com>
2023-02-04 08:49:20 -0300

    five commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,23 @@ import TextList from "./components/TextList";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const styles = {
+    background: {
+      backgroundImage:
+        "url(https://i.giphy.com/media/7VzgMsB6FLCilwS30v/giphy.gif)",
+      backgroundRepeat: "repeat",
+      
+      
+  
+     
+ 
+    },
+  };
+
   return (
-    <div>
-      <NavBar/>
-      <TextList/>
+    <div style={styles.background}>
+      <NavBar />
+      <TextList />
     </div>
   );
 }
--- src/components/TextList.jsx
+++ src/components/TextList.jsx
@@ -1,21 +1,34 @@
 import { textos as data } from "../data/textos";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import TextCard from "./TextCard";
-//import { Card } from "react-bootstrap";
 
 function TextList() {
   const [textos, setTextos] = useState([]);
 
-  useEffect(()=> {
-    setTextos(data)
-  }, [])
-  
-  if(textos.length === 0){
-    return <h1 className="text-white text-4xl font-bold text-center">No hay textos para mostrar</h1>
+  useEffect(() => {
+    setTextos(data);
+  }, []);
+
+  if (textos.length === 0) {
+    return (
+      <h1 className="text-white text-4xl font-bold text-center">
+        No hay textos para mostrar
+      </h1>
+    );
   }
 
+  const styles = {
+    background: {
+      backgroundImage:
+        "url(https://i.giphy.com/media/SdNTGnxfCT6eNqxWO8/giphy.gif)", //ITRemFlr5tS39AzQUL g7hbqhnKnewIIBbTu4 U4FkC2VqpeNRHjTDQ5 xUPGcmSyByD8BJ7pcY l3vR3LpUR3uFkFXlC
+      backgroundRepeat: "repeat",
+      backgroundSize: "100% 100%",
+      backgroundPosition: "center",
+    },
+  };
+
   return (
-    <div className="grid grid-cols-3 gap-2">
+    <div className="grid grid-cols-3 gap-2" style={styles.background}>
       {textos.map((texto) => (
         <TextCard key={texto.id} text={texto} />
       ))}
