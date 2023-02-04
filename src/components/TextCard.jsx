a3e150684f76a99b07554d97c20bdf902b4d1f22
= <raullivon@gmail.com>
2023-02-04 06:14:00 -0300

    second commit

--- src/components/TextCard.jsx
+++ src/components/TextCard.jsx
@@ -5,9 +5,10 @@ function TextCard({ text }) {
 //  const { deleteTask } = useContext(TaskContext);
 
   return (
-    <div className="bg-gray-900 text-white p-4 rounded-md">
-      <h1 className="text-xl font-bold capitalize">{text.titulo}</h1>
-      <p className="text-gray-500 text-sm p-4">{text.descripcion}</p>
+    <div className="bg-gray-900 text-white p-4 rounded-md mt-4 ml-4 mr-4 mb-4">
+      <h1 className="text-xl font-bold capitalize text-center">{text.titulo}</h1>
+      <p className="text-gray-500 text-sm p-4 text-center">{text.descripcion}</p>
+      <img className="" src={text.imagen} alt="" />
     </div>
   );
 }
